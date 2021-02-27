5398b306722c3a502e58c954b8f7e97dd513ff31
wecyhom <68796995+wecyhom@users.noreply.github.com>
2021-02-27 23:40:34 +0800

    Update login.py

--- login.py
+++ login.py
@@ -1,19 +1,21 @@
 import requests
 import re
+import urllib3
 from encrypt import wiseduAES
+urllib3.disable_warnings()
 
 
 def login(username, password):
     # 登陆地址
     url = 'https://ids.ahnu.edu.cn/authserver/login?service=https%3A%2F%2Fahnu.campusphere.net%2Fportal%2Flogin'
-    
+
     # 设置请求头
     headers = {
         'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/88.0.4324.182 Safari/537.36 Edg/88.0.705.74'
     }
-    
+
     # 获取cookies
-    res = requests.get(url=url, headers=headers)
+    res = requests.get(url=url, headers=headers, verify=False)
     cookies = requests.utils.dict_from_cookiejar(res.cookies)
 
     # 获取隐藏字段
@@ -26,7 +28,7 @@ def login(username, password):
     payload['username'] = username
     payload['password'] = (wiseduAES(password, salt['pwdDefaultEncryptSalt']))
     res = requests.post(url=url, headers=headers, cookies=cookies,
-                        data=payload, allow_redirects=False)
+                        data=payload, allow_redirects=False, verify=False)
     # 获取重定向地址
     try:
         url = res.headers['Location']
@@ -34,9 +36,8 @@ def login(username, password):
         exit('用户名或密码错误')
 
     # 获取cookies
-    res = requests.get(url=url, headers=headers, allow_redirects=False)
+    res = requests.get(url=url, headers=headers,
+                       allow_redirects=False, verify=False)
     cookies = requests.utils.dict_from_cookiejar(res.cookies)
-    
+
     return cookies
-
-
