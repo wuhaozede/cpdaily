500f5a9f99d0e0d930ee1843a1dd9d1290a70b9d
wecyhom <68796995+wecyhom@users.noreply.github.com>
2021-02-28 09:32:38 +0800

    Update fill.py

--- fill.py
+++ fill.py
@@ -1,4 +1,4 @@
-def singleItem(r1,r2):
+def singleItem(r1, r2):
     r1['value'] = r2['value']
     r1['minValue'] = 0
     r1['maxValue'] = 0
@@ -8,29 +8,29 @@ def singleItem(r1,r2):
     return r1
 
 
-def multipleItems(r1,r2):
+def multipleItems(r1, r2):
     r1['minValue'] = 0
     r1['maxValue'] = 0
-    for item in  r2['fieldItems']:
+    for item in r2['fieldItems']:
         if item['isSelected'] == 1:
             content = item['content']
     for item in r1['fieldItems']:
         if item['content'] == content:
             value = item['itemWid']
             item['isSelected'] = 1
-            r1['fieldItems'].clear()
-            r1['fieldItems'] = item
-
+            r1['fieldItems'] = []
+            r1['fieldItems'].append(item)
     r1['value'] = value
-
     return r1
-def fillForm(r1,r2):
+
+
+def fillForm(r1, r2):
     form = []
     for index in range(len(r1)):
         if r1[index]['fieldItems']:
-            item = multipleItems(r1[index],r2[index])
+            item = multipleItems(r1[index], r2[index])
             form.append(item)
         else:
-            item = singleItem(r1[index],r2[index])
+            item = singleItem(r1[index], r2[index])
             form.append(item)
     return form
