f9de0a2b5d04a3aa050f8d2861a5c3713f75879b
wuhaozede <62633045+wuhaozede@users.noreply.github.com>
2021-04-13 11:27:58 +0800

    Update submit.py

--- submit.py
+++ submit.py
@@ -32,7 +32,7 @@ def queryCollectorHistoryList(headers, cookies):
     res = requests.post(url=url, headers=headers, cookies=cookies,
                         data=json.dumps(payload))
     data = res.json()
-    wid = data['datas']['rows'][10]['wid']
+    wid = data['datas']['rows'][15]['wid']
     return wid
 
 
